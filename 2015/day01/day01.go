80579357c31eb6551f72f021f8e53930580be441
Bruno Soares <brunofpsoares25@gmail.com>
2025-10-24 20:05:07 +0100

    2015 - day 01

--- 2015/day01/day01.go
+++ 2015/day01/day01.go
@@ -1,1 +1,31 @@
 package day01
+
+func part1(input string) int {
+	floor := 0
+	for _, r := range input {
+		if r == '(' {
+			floor++
+		} else {
+			floor--
+		}
+	}
+
+	return floor
+}
+
+func part2(input string) int {
+	floor := 0
+	for i, r := range input {
+		if r == '(' {
+			floor++
+		} else {
+			floor--
+		}
+
+		if floor == -1 {
+			return i + 1
+		}
+	}
+
+	return -1
+}
