43f4b5ba329d5e247359c3f201d2d8743760661f
Bruno Soares <bruno.soares@fanduel.com>
2025-10-27 11:44:27 +0000

    2015 - day 03

---
+++ 2015/day03/day03_test.go
@@ -0,0 +1,28 @@
+package day03
+
+import (
+	"advent-of-code-go/util/tests"
+	_ "embed"
+	"testing"
+)
+
+//go:embed input.txt
+var input string
+
+var exampleInput = `^>v<`
+
+func TestPart1(t *testing.T) {
+	testCases := tests.TestInputs{
+		{"Example", exampleInput, 4},
+		{"Actual", input, 2572},
+	}
+	tests.RunWithTestCases(testCases, part1, t)
+}
+
+func TestPart2(t *testing.T) {
+	testCases := tests.TestInputs{
+		{"Example", exampleInput, 3},
+		{"Actual", input, 2631},
+	}
+	tests.RunWithTestCases(testCases, part2, t)
+}
